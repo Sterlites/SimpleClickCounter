6b47c94e0cd8bcc6ff0e6af86f66b98cf3fa4185
Sterlites <101171175+Sterlites@users.noreply.github.com>
2024-08-19 17:37:04 +0530

    ClickThe button, if you can! 🤓

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'dart:math' as math;
+
+// Customizable parameters
+const double ANIMATION_SPEED = 100.0; // Pixels per second
+const Color BUTTON_COLOR = Colors.blue; // Color of the floating button
+const double EDGE_BOUNCE_RANDOMNESS = 0.2; // 0.0 to 1.0, adds randomness to bounce angle
 
 void main() {
   runApp(const MyApp());
@@ -7,31 +13,15 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'ClickThe button, if you can! 🤓',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 34, 49, 255)),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo App'),
+      home: const MyHomePage(title: 'ClickThe button, if you can! 🤓'),
     );
   }
 }
@@ -39,83 +29,153 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _MyHomePageState extends State<MyHomePage> with SingleTickerProviderStateMixin {
   int _counter = 0;
+  late AnimationController _controller;
+  late ValueNotifier<Offset> _position;
+  late ValueNotifier<Offset> _velocity;
+
+  @override
+  void initState() {
+    super.initState();
+    _position = ValueNotifier(Offset.zero);
+    _velocity = ValueNotifier(_getRandomVelocity());
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 1),
+    )..repeat();
+
+    _controller.addListener(_updatePosition);
+  }
+
+  Offset _getRandomVelocity() {
+    final angle = math.Random().nextDouble() * 2 * math.pi;
+    return Offset(math.cos(angle), math.sin(angle));
+  }
+
+  void _updatePosition() {
+    final size = MediaQuery.of(context).size;
+    final buttonSize = 156.0;
+    final maxX = size.width - buttonSize;
+    final maxY = size.height - buttonSize - kToolbarHeight;
+
+    var newX = _position.value.dx + _velocity.value.dx * ANIMATION_SPEED * _controller.value;
+    var newY = _position.value.dy + _velocity.value.dy * ANIMATION_SPEED * _controller.value;
+
+    var newVelocityX = _velocity.value.dx;
+    var newVelocityY = _velocity.value.dy;
+
+    bool bounced = false;
+
+    if (newX < 0 || newX > maxX) {
+      newVelocityX *= -1;
+      newX = newX < 0 ? 0 : maxX;
+      bounced = true;
+    }
+
+    if (newY < 0 || newY > maxY) {
+      newVelocityY *= -1;
+      newY = newY < 0 ? 0 : maxY;
+      bounced = true;
+    }
+
+    if (bounced) {
+      // Add some randomness to the bounce angle
+      final currentAngle = math.atan2(newVelocityY, newVelocityX);
+      final randomAngleChange = (math.Random().nextDouble() - 0.5) * EDGE_BOUNCE_RANDOMNESS * math.pi;
+      final newAngle = currentAngle + randomAngleChange;
+      final speed = math.sqrt(newVelocityX * newVelocityX + newVelocityY * newVelocityY);
+      newVelocityX = speed * math.cos(newAngle);
+      newVelocityY = speed * math.sin(newAngle);
+    }
+
+    _position.value = Offset(newX, newY);
+    _velocity.value = Offset(newVelocityX, newVelocityY);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    _position.dispose();
+    _velocity.dispose();
+    super.dispose();
+  }
 
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Transform(
-          transform: Matrix4.diagonal3Values(1, 0.9, 1),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                'You have pushed the button this many times:',
-                style: TextStyle(
-                  fontFamily: 'Arial',
-                  fontSize: 20,
-                  color: Colors.black87,
-                  shadows: [
-                    Shadow(
-                      color: Colors.black.withOpacity(0.5),
-                      blurRadius: 4,
-                    ),
-                  ],
+      body: Stack(
+        children: [
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'You have pushed the button this many times:',
+                  style: TextStyle(
+                    fontFamily: 'Verdana',
+                    fontSize: 20,
+                    color: Colors.black87,
+                    shadows: [
+                      Shadow(
+                        color: Colors.red.withOpacity(0.5),
+                        blurRadius: 10,
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-            ],
+                Text(
+                  '$_counter',
+                  style: TextStyle(
+                    fontSize: 80,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.redAccent,
+                    fontFamily: 'Roboto',
+                    shadows: [
+                      Shadow(
+                        color: Colors.black.withOpacity(0.3),
+                        offset: Offset(2, 2),
+                        blurRadius: 4,
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
-        ),
+          ValueListenableBuilder<Offset>(
+            valueListenable: _position,
+            builder: (context, position, child) {
+              return Positioned(
+                left: position.dx,
+                top: position.dy + kToolbarHeight,
+                child: FloatingActionButton(
+                  onPressed: _incrementCounter,
+                  tooltip: 'Increment',
+                  backgroundColor: BUTTON_COLOR,
+                  child: const Icon(Icons.add),
+                ),
+              );
+            },
+          ),
+        ],
       ),
-      
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
